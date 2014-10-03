9e75b456a86757418522432cb26aff3062499272
hahnicity <grehm87@gmail.com>
2014-10-02 23:24:47 -0700

    Commit all the things!

--- README.md
+++ README.md
@@ -3,6 +3,32 @@ appraiser
 
 A data project to see if we can predict housing prices in select areas of the Bay Area
 
+Predictions
+-----------
+For what I now realize is a very hard problem with tons of variable inputs and
+human subjection involved I'm actually pretty pleased with my results.
+
+I learned
+
+ * How to architect a data gathering service
+ * Basics of feature engineering
+ * Fun ways to get around API rate limiting
+ * Network sleuthing to find undocumented APIs
+
+All in all it was a fun project. Here are my results of a 141 member training set.
+
+![Results](results.png)
+
+Why Finished?
+-------------
+I feel like I accomplished my original goal, which was to create an introductory
+machine learning project. When I realized how much more work I would need to do
+to actually get the project into working state I chafed at putting multiple months
+of time into developing a richer feature set. This was only meant to be a several week
+project but it expanded into a 3 week one. Also I am interested in taking another class
+on this subject so I don't think I have time to fully complete this project. Maybe
+I will pick this up later. This work surely isn't going anywhere.
+
 Take Aways
 ----------
 
@@ -15,3 +41,5 @@ Take Aways
    hard to tell from the basic data given to us through zillow. Complete feature representation
    for homes at least should probably come from a variety of sources, not the least user surveys
    about an area.
+ * You can have a bunch of data but if your model is insufficient then adding more data won't 
+   really help your problem of having a high error.
--- analyze.m
+++ analyze.m
@@ -34,22 +34,54 @@ function [theta, j_history] = perform_gradient_descent(x, y, theta, alpha, lambd
 end
 
 
+function lambda = determine_best_lambda(training_set, cv_set, y_train, y_cv)
+    lambda_vals = [1 2 4 10 40 100 300 500 800];
+    j_train = [];
+    j_cv = [];
+    for lambda = lambda_vals
+        theta = zeros(size(training_set)(2), 1);
+        [theta, j_history] = perform_gradient_descent(training_set, y_train, theta, .1, lambda, 1000);
+        j_cv(length(j_cv) + 1) = compute_cost(cv_set, y_cv, theta);
+        j_train(length(j_train) + 1) = j_history(length(j_history));
+    end
+    [_, idx] = min(j_cv - j_train);
+    lambda = lambda_vals(idx);
+end
+
+
 function main()
     data = load("appraiser/home-data.csv");
     x = data(:, 1:size(data)(2) - 1);
     y = data(:, size(data)(2));
     m = length(y);
+    training_set_length = ceil(m * .6);
+    if (mod(length(x) - training_set_length, 2) == 1)
+        cv_set_length = ceil((m - training_set_length) / 2);
+    else
+        cv_set_length = (m - training_set_length) / 2;
+    end
     x_normed = normalize_x(x);
     x = [ones(m, 1), x_normed];
+    training_set = x(1:training_set_length, :);
+    cv_set = x(training_set_length:training_set_length + cv_set_length, :);
+    test_set = x(training_set_length + cv_set_length:m, :);
+    lambda = determine_best_lambda(training_set, ...
+                                   cv_set, ...
+                                   y(1:training_set_length), ...
+                                   y(training_set_length:training_set_length + cv_set_length));
     theta = zeros(size(x)(2), 1);
-    [theta, j_history] = perform_gradient_descent(x, y, theta, .01, 0, 10000);
-    predictions = x * theta;
-    idx = [1:m];
-    plot(idx, y, "color", "red")
+    [theta, _] = perform_gradient_descent(training_set, y(1:training_set_length), theta, .1, lambda, 10000);
+    predictions = (test_set * theta)';
+    idx = [1:length(predictions)];
+    plot(idx, y(training_set_length + cv_set_length:m)', "color", "red");
     hold on;
     plot(idx, predictions, "color", "blue");
+    xlabel("House index")
+    ylabel("Price")
+    legend("Predicted Prices", "Actual Prices")
+    axis([1 length(idx)]);
+    print -dpng results.png
     pause;
-    j_history(length(j_history))
 end
 
 
